99c22877490dd63a404a3e391d0e944c8399b1cd
Joshua Collins <joshua.collins@postgrad.manchester.ac.uk>
2023-03-23 11:04:21 +0000

    Added updated files

--- README.md
+++ README.md
@@ -1,7 +1,191 @@
-# steel_dilatometer_analysis
-Analysis codes for steel alloy dilatometry experiments.
+# Steel Dilatometry Analysis Code
 
-From measuring transformation start temperatures using raw dilatometry data, to using that data to plot continuous cooling transformation (CCT) curves. This directory has it all!
+Python code for analysing steel dilatometer curves. Functions for the code are found in "dilatometry_ts_calculator.py" and include: 'dilatometry_curve_plotter' for plotting a temperature-strain curve, 'offset_method' for measuring transformation start temperatures, $T_\mathrm{s}$, using an offset method, and 'second_deriv_method' which is a second technique for measuring $T_\mathrm{s}$ by finding the inflection point in a curve.
 
-# Cite Us!
-[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7303894.svg)](https://doi.org/10.5281/zenodo.7303894)
+## 1. Functions
+
+### Dilatometry Curve Plotter
+
+A function for plotting the measured dilatometry temperature vs. strain curve. Output is a temperature-strain graph of the dilatometry data.
+
+    dilatometry_curve_plotter(file,L0,N)
+  
+**Inputs:**
+
+    file, L0, N
+    
+**file**: raw dilatometry data in .asc file format. Raw data should be saved with "Temperature" in the first column and "Change in Length" in the second column (ignoring the index column). File name (+ datapath to file) should be inputted as a string. 
+
+**L0**: intial length of the dilatometer sample in meters, m. Initial length should be inputted as a float.
+
+**N**: row index number to start plotting the data from. Ideally used to isolate cooling data (i.e., the tail end of the dataset). Index number should be inputted as a integer.
+
+**Example:**
+
+Fig. 1 shows an example output for a bainitic steel dilatometry cooling curve. The original sample length, L0, was 0.01 m and the cooling curve data started at index value, N, 12000.
+
+Example inputs:
+
+    file = '210423_SteelDilatometryData_test1.asc'
+    
+    L0 = 0.01
+    
+    N = 12000
+    
+Example code:
+    
+    dilatometry_curve_plotter(file='210423_SteelDilatometryData_test1.asc', L0=0.01, N=12000)
+    
+Example output:
+
+<figure>
+  <img
+  src="example figures/dilatometry_curve_plotter_EXAMPLE.png"
+  alt="."
+  width="80%" 
+  height="80%">
+  <figcaption>Fig. 1 An example dilatometry curve output by the 'dilatometry_curve_plotter' function. In this example, N is set to a value at the start of the cooling curve as to isolate this section of the data.</figcaption>
+</figure>
+
+&nbsp;
+    
+### Offset Method
+
+A function for analysing the dilatometer cooling curve transformations using an offset method, similar to the technique described by Yang and Bhadeshia [1]. A total of three separate temperature ranges are chosen to measure three different $T_\mathrm{s}$ values. Output is three temperature-strain curves for each temperature range tested, plus a Python list giving an average $T_\mathrm{s}$ and standard deviation separated by a comma (i.e., [Ts, std]).
+
+    offset_method(file,L0,Nc,Ti,dT,comp,p,X)
+
+**Inputs:**
+
+    file, L0, Nc, Ti, dT, comp, p, X
+    
+**file**: raw .asc dilatometer data (as detailed above).
+
+**L0**: original length of dilatometer sample (as detailed above).
+
+**Nc**: row index value for the start of the cooling curve. If data before cooling is used then this could disrupt the function and its ability to calculate $T_mathrm{s}$. N should be inputted as an integer.
+    
+**Ti**: the lower temperature value used for calculating the gradient of a linear section of the dilatometer curve. A gradient will be taken over a 50&deg;C range. It is recommended this value is at least 50&deg;C higher than the measured $T_\mathrm{s}$ value. Ti should be inputted as a float/integer.
+
+**dT**: the temperature difference between the ranges of temperatures used to calculate gradients. Three ranges of temperatures are used to calculate three separate gradient values to then measured three separate $T_\mathrm{s}$ values. dT is the temperature difference between these equally spaced ranges. dT should be inputted as a float/integer.
+
+**comp**: the composition of the steel alloy in wt.%. This input is used to calculate the exact offset induced by a transformation to either ferrite, 'f', or martensite, 'm', in austenite. Composition should be inpuuted in dictionary format.
+
+**p**: the phase/constituent forming in austenite. This should be either ferrite, 'f', or martensite, 'm'. p should be inputted as a string.
+
+**X**: the mole fraction of phase/constituent forming in austenite. X should be inputted as a float.
+    
+**Explanation of the Offset Method:**
+
+The function calculates $T_\mathrm{s}$ using an offset method. This technique first calculates the gradient of the linear section of the dilatometry curve (at least 50&deg;C before the cooling transformation). A 50&deg;C range is selected for this. This gradient is then used to plot an offset line, where $T_\mathrm{s}$ is measured at its interception with the cooling curve. To reduce uncertainty, three 50&deg;C temperature ranges are used and an average $T_\mathrm{s}$ is found. The position of these temperature ranges is dictated by the **Ti** and **dT** inputs. The **Ti** input dictates where the first 50&deg;C range will start from, whereas **dT** dictates the temperature distance between these ranges. Fig. 2 shows a visualisation of this.
+
+<figure>
+  <img
+  src="example figures/selecting_gradient_ranges.png"
+  alt="."
+  width="50%" 
+  height="50%">
+  <figcaption>Fig. 2 A schematic showing how the 'offset_method' function takes the inputs Ti and dT to select temperature ranges for gradient calculation.</figcaption>
+</figure>
+
+&nbsp;
+
+Once a gradient has been measured, an offset line is defined. The amount this line is offset from the original curve is dependent on the temperature of transformation, the composition of the steel, **comp**, the phase/constituent that is forming in the austenite, **p**, and the mole fraction of this new phase/constituent, **X**. The code will use these inputs to calculate the exact amount of strain induced by **X** fraction of phase/constituent, **p**, within the austenite with composition, **comp**, at a specific temperature. [Note: the code does not require the input of temperature here as it will already be considered.] If the transformation is ferritic, pearlitic or bainitic, **p** = 'f'. If the transformation is martensitic, **p** = 'm'. 
+
+**Example:**
+
+Fig. 3 shows an example output for the 'offset_method' function. The technique was used on the same data plotted in Fig. 1. All inputs are true except the input for comp which has been altered for simplicity.
+
+Example inputs:
+
+        file = '210423_SteelDilatometryData_test1.asc'
+        
+        L0 = 0.01
+        
+        Nc = 12000
+        
+        Ti = 550
+        
+        dT = 50
+        
+        comp = {'C': 0.2, 'Si': 0.1, 'Mn': 0.3}
+        
+        p = 'f'
+        
+        X = 0.01
+        
+Example code:
+
+        offset_method(file='210423_SteelDilatometryData_test1.asc', L0=0.01, Nc=12000, Ti=550, dT=50, comp={'C': 0.2, 'Si': 0.1, 'Mn': 0.3}, p='f', X=0.01)
+        
+Example output:
+
+        [463.5, 1.7]
+
+<figure>
+  <img
+  src="example figures/offset_method_EXAMPLE.png"
+  alt="."
+  width="100%" 
+  height="100%">
+  <figcaption>Fig. 3 An example output by the 'offset_method' function.</figcaption>
+</figure>
+
+&nbsp;
+
+### Second Derivative Method
+
+A function for analysing the dilatometer cooling curve transformations by finding the inflection point of a curve. This technique is ideal when the offset method cannot be used - typically during a double transformation where there is no linear section before the 2nd transition. The output is a 1d gaussian filtered temperature-strain curve showing the measured $T_\mathrm{s}$ position, a temperature-2nd derivative curve, and a Python list giving an average $T_\mathrm{s}$ and standard deviation separated by a comma (i.e., [Ts, std]).
+
+    second_deriv_method(file,L0,Nc,s,Trange)
+
+**Inputs:**
+
+    file, L0, Nc, s, Trange
+    
+**file**: raw .asc dilatometer data (as detailed above).
+
+**L0**: original length of dilatometer sample (as detailed above).
+
+**Nc**: row index number for the start of the cooling curve (as detailed above).
+
+**s**: sigma value for the 1d gaussian filter (i.e., curve smoothing factor). The raw dilatometry data needs to be smoothed in order to better isolate inflection points within the curve. Higher values of sigma will increase the intensity of data smoothing. Sigma should be inputted as a float/integer.
+
+**Trange**: the temperature range in which to look for an inflection point. Dilatometry curves will contained a multitude of inflection points so users are asked to provide a temperature range in which to look for a specific inflection point (i.e., at the $T_\mathrm{s}$). The temperature range should be inputted as a Python list with a minimum temperature, T1, and maximum temperature, T2, separated by a comma - as so [T1, T2].
+
+**Example:**
+
+Fig. 4 shows an example output for the 'second_deriv_method' function.
+
+Example inputs:
+
+        file = '210423_SteelDilatometryData_test1.asc'
+        
+        L0 = 0.01
+        
+        Nc = 12000
+        
+        s = 3
+        
+        Trange = [620, 670]
+        
+Example code:
+
+        second_deriv_method(file='210423_SteelDilatometryData_test1.asc', L0=0.01, Nc=12000, s=3, Trange=[620,670])
+        
+Example output:
+
+        [645.5, 0.5]
+        
+<figure>
+  <img
+  src="example figures/second_deriv_method_EXAMPLE.png"
+  alt="."
+  width="75%" 
+  height="75%">
+  <figcaption>Fig. 4 An example output by the 'second_deriv_method' function.</figcaption>
+</figure>
+
+## 2. Bibliography
+
+[1]:  H-S. Yang and H. K. D. H. Bhadeshia. Uncertainties in dilatometric determination of martesite start temperature. Materials Science and Technology, 23:556–560, 2007
---
+++ dilatometry_ts_calculator.py
@@ -0,0 +1,302 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy
+from scipy.ndimage import gaussian_filter1d
+
+def mass2mole(mass):
+    # define molecular weights of elements
+    molweight = {'Fe':55.85,'C':12.01,'Si':28.09,'Mn':54.94,'Ni':58.69,'Cr':52,'Mo':95.94,'W':183.85,'Co':58.93,'V':50.94,'Nb':92.91,'Cu':63.55,'Al':26.98,'Ti':47.88,'O':16,'N':14.01,'B':10.81,'P':30.97,'S':32.06,'As':74.92}
+    
+    # check that all elements are considered, if not included set comp of element = 0
+    if 'Fe' in mass.keys():
+        pass
+    else:
+        mass['Fe'] = 100 - sum(mass.values())
+    for e in molweight:
+        if e in mass.keys():
+            pass
+        else:
+            mass[e] = 0
+            
+    # convert weight percent to mole fraction
+    a = []
+    for elm in mass.keys():
+        mole = mass[elm]/molweight[elm]
+        a.append(mole)
+    tot_moles = sum(a)
+    moles = {}
+    n = 0
+    for mol in a:
+        moles[list(mass.keys())[n]] = mol/tot_moles
+        n += 1
+    return moles
+
+def perc2frac(wperc):
+    # convert weight percent to weight fraction
+    wegtf = {}
+    for e in wperc:
+        wegtf[e] = wperc[e]/100
+    return wegtf
+
+def a_f0(molef):
+    # check that all elements are considered, if not included set comp of element = 0
+    elements = ['C','Si','Mn','Ni','Cr','Mo','V']
+    for e in elements:
+        if e in molef.keys():
+            pass
+        else:
+            molef[e] = 0
+            
+    # calculate ferrite lattice parameter in nm at 298K
+    aFe = 0.28664 
+    return aFe + ( ((aFe-(0.0279*molef['C']))**2)*(aFe+(0.2496*molef['C'])) - (aFe**3) )/(3*(aFe**2)) - (0.003*molef['Si']) + (0.006*molef['Mn']) + (0.007*molef['Ni']) + (0.031*molef['Mo']) + (0.005*molef['Cr']) + (0.0096*molef['V'])
+
+def a_a0(wegtf):
+    # check that all elements are considered, if not included set comp of element = 0
+    elements = ['C','Mn','Ni','Cr','N','Al','Co','Cu','Mo','Nb','Ti','V','W']
+    for e in elements:
+        if e in wegtf.keys():
+            pass
+        else:
+            wegtf[e] = 0
+            
+    # calculate austenite lattice parameter in nm at 298K
+    return 0.3573 + (0.33*wegtf['C']) + (0.0095*wegtf['Mn']) - (0.002*wegtf['Ni']) + (0.006*wegtf['Cr']) + (0.22*wegtf['N']) + (0.056*wegtf['Al']) - (0.004*wegtf['Co']) + (0.015*wegtf['Cu']) + (0.031*wegtf['Mo']) + (0.051*wegtf['Nb']) + (0.039*wegtf['Ti']) + (0.018*wegtf['V']) + (0.018*wegtf['W'])
+
+def latt_param(comp,p,T):
+    # check if inputs are valid
+    if p not in ['f','a','m']:
+        return print('Invalid microstructure (p) defined. Valid inputs include: f - ferrite, a - austenite, m - martensite.')
+    
+    # convert comp (in wt.%) to mole fraction (molef) and weight fraction (wegtf)
+    
+    # austenite weight fraction (wegtf_a)
+    wegtf_a = perc2frac(comp)
+
+    # ferrite mole fraction (molef_f) assuming 0.03 wt.% C in solution
+    wperc_f = copy.deepcopy(comp)
+    wperc_f['C'] = 0.03
+    molef_f = mass2mole(wperc_f)
+
+    # martensite mole fraction (molef_m) assuming supersaturated w/ C
+    wperc_m = copy.deepcopy(comp)
+    molef_m = mass2mole(wperc_m)
+    
+    # define coefficients of thermal expansion (K-1) for ferrite, f, and austenite, a
+    alpha = {'f':1.59*10**(-5),'a':2.03*10**(-5),'m':1.59*10**(-5)}
+    
+    # define initial lattice parameters @ RT (298K) 
+    a0 = {'f':a_f0(molef_f),'a':a_a0(wegtf_a),'m':a_f0(molef_m)}
+    
+    # calculate lattice parameter at T (in Celcius)
+    return ((alpha[p]*(T-25))+1)*a0[p]
+
+def offset_Calc(comp,p,T,X):
+    
+    # define number of atoms in bcc and fcc unit cells
+    N = {'f':2,'a':4,'m':2}
+    
+    # calculate the volume, V, of the new system (i.e., X amount of microstructure in austenite)
+    V = ((1-X)*(1/N['a'])*(latt_param(comp,'a',T)**3)) + (X*(1/N[p])*(latt_param(comp,p,T)**3))
+    
+    # calculate the volume of the austenite, V0
+    V0 = (1/N['a'])*(latt_param(comp,'a',T)**3)
+    
+    # calculate the strain difference of adding X fraction of new microstructure, p, into austenite at temperature, T
+    return ((V**(1/3))/(V0**(1/3)))-1
+
+def dilatometry_curve_plotter(file,L0,N):
+    # read data in file
+    data = pd.read_csv(file, delimiter = r"\s+", header = None, skiprows=4, engine='python',encoding= 'unicode_escape')
+    
+    # isolate first 2 columns (which should be 'Temperature' & 'Change in Length')
+    for n in range(len(data.columns)):
+        if n == 1 or n == 2:
+            pass
+        else:
+            del data[n]
+    data.columns = ['T', 'x']
+    
+    # calculate strain using initial length (L0)
+    data['e'] = (data['x']*10**(-6))/L0
+    # isolate cooling curve from data
+    dataset = data[N:]
+    
+    # plot cooling curve as 'Temperature' vs. 'Strain'
+    plt.figure(figsize=(15,7))
+    plt.plot(dataset['T'],dataset['e'],color='mediumseagreen',linewidth=2,label='Dilatometry Curve')
+    plt.xlabel('Temperature (\N{DEGREE SIGN}C)', fontsize=16)
+    plt.ylabel('Strain (mm/mm)', fontsize=16)
+    plt.tick_params(axis='x', labelsize=13)
+    plt.tick_params(axis='y', labelsize=13)
+    plt.legend(loc='upper left',fontsize=13)
+    return
+
+def offset_method(file,L0,Nc,Ti,dT,comp,p,X):
+    # read data in file
+    data = pd.read_csv(file, delimiter = r"\s+", header = None, skiprows=4, engine='python',encoding= 'unicode_escape')
+    
+    # isolate first 2 columns (which should be 'Temperature' & 'Change in Length')
+    for n in range(len(data.columns)):
+        if n == 1 or n == 2:
+            pass
+        else:
+            del data[n]
+    data.columns = ['T', 'x']
+    # calculate strain using initial length (L0)
+    data['e'] = (data['x']*10**(-6))/L0
+    
+    # isolate cooling curve from data
+    dataset = data[Nc:]
+    
+    # isolate temperature and strain data as Python lists
+    temp = list(dataset['T'])
+    strain = list(dataset['e'])
+
+    # define temperature ranges for offset lines using Ti and dT
+    temp_range = [Ti,Ti+dT,Ti+(2*dT)]
+    # create empty list for interception points to be appended to
+    interceptions = []
+    
+    # create figure space to plot into
+    fig, axs = plt.subplots(1, 3, figsize = (16,5))
+    
+    # set count start (i = 0)
+    i = 0
+    # loop through temperature ranges and calculate interception points, appending to pre-defined list
+    for T1 in temp_range:
+        # determine index values (n) for temperature range (allowing direct relation to strain list)
+        n0, n1 = None, None
+        j1, j2 = 0, 0
+        while n1 == None:
+            try:
+                n1 = list(np.around(temp,0)).index(round(T1+j1,0))
+            except ValueError:
+                j1 += 1
+        
+        while n0 == None:
+            try:
+                n0 = list(np.around(temp,0)).index(round(T1+50+j2,0))
+            except ValueError:
+                j2 += 1
+        
+        # define equation for straight line (y = m.x + c) by calculating m and c using temperature range
+        m = (strain[n0]-strain[n1])/(temp[n0]-temp[n1])
+        c = strain[n1] - m*temp[n1]
+        
+        # create empty lists for the straight line data and offset line data
+        lin_strain, off_strain = [], []
+        
+        # loop through temperature values to fill lists with data
+        for T in temp:
+            lin_strain.append(m*T + c)
+            
+        # determine start of transformation (i.e, when 0 < X < 0.01)
+        idx1 = np.argwhere(np.gradient(np.sign(np.array(lin_strain) - np.array(strain)))).flatten() 
+        To = temp[idx1[-1]]
+        
+        # calculate offset at To
+        offset = offset_Calc(comp,p,To,X)
+        
+        # loop through temperature values to fill lists with data
+        for T in temp:
+            off_strain.append(m*T + c + offset)
+        # determine intersection points between cooling curve ('strain') and offset line ('off_strain')
+        idx2 = np.argwhere(np.gradient(np.sign(np.array(off_strain) - np.array(strain)))).flatten() 
+        intercept = temp[idx2[-1]]
+        
+        # append temperature of interception to list
+        interceptions.append(intercept)
+        
+        # plot analysis on graph for user visuals
+        axs[i].plot(temp,strain,color='mediumseagreen',zorder=0,label='Dilatometry Curve')
+        axs[i].plot(temp,lin_strain,color='k',linestyle=':',zorder=1,label='Gradient Line')
+        axs[i].plot(temp,off_strain,color='steelblue',linestyle='-',zorder=2,label='Offset Line')
+        axs[i].scatter(intercept,m*intercept + c + offset,color='tomato',marker='x',label='$T_\mathrm{s}$')
+        axs[i].text(intercept+25, m*intercept + c + offset, str(round(intercept,1))+'\N{DEGREE SIGN}C', fontsize = 12, bbox={'facecolor': 'white'}, color = 'red')
+        axs[i].set_xlabel('Temperature (\N{DEGREE SIGN}C)',fontsize=14)
+        axs[0].set_ylabel('Strain (mm/mm)',fontsize=14)
+        axs[i].legend(loc='best',fontsize=11.5)
+        axs[i].set_title('Dilatometry Curve \n Gradient range: '+str(T1)+' - '+str(T1+50)+'\N{DEGREE SIGN}C',fontsize=15)
+        minX, maxX = T1-200, T1+50
+        axs[i].set_xlim(minX,maxX)
+        axs[i].set_ylim(m*(minX)+c,m*(maxX)+c)
+        # increase count
+        i += 1
+        
+    # calculate and return the average interception temperature (i.e., Ts) and standard deviation as list -> [Ts,Std]
+    return [round(np.mean(interceptions),1), round(np.std(interceptions),1)]
+
+def second_deriv_method(file,L0,Nc,s,Trange):
+
+    # read data
+    data = pd.read_csv(file, delimiter = r"\s+", header = None, skiprows=4, usecols = range(1,3), engine='python', encoding= 'unicode_escape')
+    data.columns = ['T', 'x']
+    data['e'] = (data['x']*10**(-6))/L0
+
+    # isolate even spacing between temperature values (i.e., a 1C spacing) - this is for calculating the gradient later
+    x, y = np.array(data['T'][Nc:]), np.array(data['e'][Nc:])
+    yi = []
+    n0 = 0
+    xi = np.linspace(int(max(x)),int(min(x)),(int(max(x))-int(min(x)))+1)
+    for T in xi:
+        ys = []
+        for n in range(n0,len(x)):
+            if int(x[n]) == T:
+                ys.append(y[n])
+                n0 = n
+            if int(x[n]) < T:
+                break
+        if len(ys) == 0:
+            yi.append(yi[-1])
+        elif len(ys) > 0:
+            yi.append(np.mean(ys))
+
+    # smooth data using a Gaussian filter using sigma, s
+    smooth = gaussian_filter1d(yi, s)
+
+    # compute second derivative of smoothed data
+    smooth_d2 = np.gradient(np.gradient(smooth))
+
+    fig, axs = plt.subplots(1, 2, figsize = (11,5))
+
+    # find position of start and finish temperatures in Trange
+    n2, n1 = list(xi).index(Trange[0]), list(xi).index(Trange[-1])
+
+    # crop data around start and finish temperatures, Trange
+    xi_crop, yi_crop, smooth_crop, smooth_d2_crop = xi[n1:n2], yi[n1:n2], smooth[n1:n2], smooth_d2[n1:n2]
+
+    # calculate the temperature/s when 2nd derivative = 0
+    y0 = np.linspace(0,0,len(xi[n1:n2]))
+    idx = np.argwhere(np.gradient(np.sign(y0 - smooth_d2_crop))).flatten() 
+    intercepts = []
+
+    # average temperatures when the 2nd derivative = 0, and find standard deviation
+    for n in range(len(idx)):
+        if smooth_d2_crop[idx[n]-1] < smooth_d2_crop[idx[n]]:
+            intercepts.append(xi_crop[idx[n]])
+    interception_point = np.mean(intercepts)
+    std = np.std(intercepts)
+
+    # plot dilatometry cooling curve, the Gaussian (smoothed) curve and the interception point
+    n3, n0 = list(xi).index(int(interception_point-50)), list(xi).index(int(interception_point+50))
+    axs[0].scatter(xi[n0:n3], yi[n0:n3], marker='.',color='mediumseagreen',s=7.5,zorder=1, label='Raw Data')
+    axs[0].plot(xi[n0:n3], smooth[n0:n3],color='k',zorder=0, label='Gaussian Filtered (σ='+str(s)+')',linewidth=1)
+    axs[0].errorbar(interception_point,yi_crop[list(xi_crop).index(int(interception_point))], marker = 'x', color = 'r', xerr = std, ecolor = 'k', capsize=5, linestyle="None",zorder=2,label='$T_\mathrm{s}$')    
+    axs[0].legend(loc='best',fontsize=11.5)
+    axs[0].set_ylabel('Strain, ε (mm/mm)',fontsize=14)
+    axs[0].set_xlabel('Temperature (\N{DEGREE SIGN}C)',fontsize=14)
+    axs[0].set_title('Dilatometry Curve',fontsize=15)
+
+    # plot second derivative data and intercept point found using the Gaussian curve
+    axs[1].plot(xi_crop, smooth_d2_crop,label='$\mathrm{d}^2ε/\mathrm{d}T^2$')
+    axs[1].errorbar(interception_point,0, marker = 'x', color = 'r', xerr = std, ecolor = 'k', capsize=5, linestyle="None", zorder=2,label='$T_\mathrm{s}$')
+    axs[1].text(interception_point-5, (min(smooth_d2_crop))*(0.35), str(round(interception_point,1))+'±'+str(round(std,1))+'\N{DEGREE SIGN}C', fontsize = 13, bbox={'facecolor': 'white'}, color = 'red')
+    axs[1].set_ylabel('$\mathrm{d}^2ε/\mathrm{d}T^2$',fontsize=14)
+    axs[1].set_xlabel('Temperature (\N{DEGREE SIGN}C)',fontsize=14)
+    axs[1].set_title('Second Derivative Curve',fontsize=15)
+    axs[1].legend(loc='upper right',fontsize=11.5)
+    
+    # return Ts and standard deviation
+    return [round(interception_point,1), round(std,1)]
